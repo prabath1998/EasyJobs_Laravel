e2323e22116fb4c81fbe341126eaea1780aa1fa6
prabath1998 <prabhathudayanga1998@gmail.com>
2022-10-02 10:16:28 +0530

    listing controller added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,19 +18,8 @@ use App\Models\Listing;
 */
 
 //All listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all(),
-    ]);
-});
+Route::get('/', [ListingController::class,'index']);
 
 
 //Single listing
-Route::get('/listing/{listing}', function (Listing $listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]);
-
-
-});
+Route::get('/listing/{listing}',  [ListingController::class,'show']);
